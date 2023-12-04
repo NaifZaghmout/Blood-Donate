0683266bea0045027ae08c76b10684a2dc663dc9
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-04 15:11:10 +0000

    edit my apps.js and made the rest of setup

--- blood-donate/src/App.js
+++ blood-donate/src/App.js
@@ -1,25 +1,26 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home/Home';
-import AboutUs from './AboutUs/AboutUs';
-import ContactUs from './ContactUs/ContactUs';
-import UserSignupLogin from './UserSignupLogin/UserSignupLogin';
-import StaffSignupLogin from './StaffSignupLogin/StaffSignupLogin';
-import NotFound from './NotFound/NotFound';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import AboutUs from './components/AboutUs/AboutUs';
+import ContactUs from './components/ContactUs/ContactUs';
+import UserSignupLogin from './components/UserSignupLogin/UserSignupLogin';
+import StaffSignupLogin from './components/StaffSignupLogin/StaffSignupLogin';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/aboutus" component={AboutUs} />
-        <Route path="/contactus" component={ContactUs} />
-        <Route path="/user" component={UserSignupLogin} />
-        <Route path="/staff" component={StaffSignupLogin} />
-        <Route component={NotFound} />
-      </Switch>
-    </Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/aboutus" element={<AboutUs />} />
+    <Route path="/contactus" element={<ContactUs />} />
+    <Route path="/user" element={<UserSignupLogin />} />
+    <Route path="/staff" element={<StaffSignupLogin />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</Router>
+
   );
 }
 
