9403f83f90bb0cf499d96699d22717648178f9c5
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-05 14:12:05 +0000

    edit my setup / add index.js

--- blood-donate/src/App.js
+++ blood-donate/src/App.js
@@ -1,26 +1,29 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import AboutUs from './components/AboutUs/AboutUs';
-import ContactUs from './components/ContactUs/ContactUs';
-import UserSignupLogin from './components/UserSignupLogin/UserSignupLogin';
-import StaffSignupLogin from './components/StaffSignupLogin/StaffSignupLogin';
-import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import UserSignupLogin from './User-Singup-Login'; // Adjust the path
+import StaffSignupLogin from './Staff-Singup-Login'; // Adjust the path
 
 function App() {
   return (
     <Router>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/aboutus" element={<AboutUs />} />
-    <Route path="/contactus" element={<ContactUs />} />
-    <Route path="/user" element={<UserSignupLogin />} />
-    <Route path="/staff" element={<StaffSignupLogin />} />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-</Router>
-
+      <div>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/user" element={<UserSignupLogin />} />
+            <Route path="/staff" element={<StaffSignupLogin />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- blood-donate/src/components/DonorForm/DonorForm.styled.ts
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-
-export const DonorFormWrapper = styled.div`
-`;
