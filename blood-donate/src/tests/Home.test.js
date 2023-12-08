c2f53ca911fe08545eafec1736f958f56525a263
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-08 16:04:17 +0000

    re-setup the app

--- blood-donate/src/tests/Home.test.js
+++ blood-donate/src/tests/Home.test.js
@@ -1,26 +1,27 @@
-// src/test/home.test.js
+// src/tests/home.test.js
 
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
-import Home from '../components/home';
+import Home from '../components/Home';
 
 describe('Home Component', () => {
-    beforeEach(() => {
-        render(<Home />);
-    });
+  beforeEach(() => {
+    render(<Home />);
+  });
 
-    test('renders welcome message', () => {
-        const welcomeMessage = screen.getByText(/Welcome to Stockholm Blood Donate Organization/i);
-        expect(welcomeMessage).toBeInTheDocument();
-    });
+  test('renders welcome message', () => {
+    const welcomeMessage = screen.getByText(/Welcome to Stockholm Blood Donate Organization/i);
+    expect(welcomeMessage).toBeInTheDocument();
+  });
 
-    test('renders blood donation sections', () => {
-        const maleDonationSection = screen.getByText(/Donating Blood as a Male/i);
-        const femaleDonationSection = screen.getByText(/Donating Blood as a Female/i);
-        expect(maleDonationSection).toBeInTheDocument();
-        expect(femaleDonationSection).toBeInTheDocument();
-    });
+  test('renders blood donation sections', () => {
+    const maleDonationSection = screen.getByText(/Donating Blood as a Male/i);
+    const femaleDonationSection = screen.getByText(/Donating Blood as a Female/i);
+    expect(maleDonationSection).toBeInTheDocument();
+    expect(femaleDonationSection).toBeInTheDocument();
+  });
 
-    
+ 
 
 });
