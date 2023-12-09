62756d59d6385ad5f57df37c7a9e859f276f6d51
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-09 08:11:15 +0000

    add basic rendering test for aboutus.js page

--- blood-donate/src/tests/AboutUs.test.js
+++ blood-donate/src/tests/AboutUs.test.js
@@ -2,3 +2,32 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import AboutUs from '../components/AboutUs'; 
+
+describe('AboutUs Component', () => {
+    test('renders the main heading and organization description', () => {
+      render(<AboutUs />);
+  
+      expect(screen.getByText('About Our Organization')).toBeInTheDocument();
+      expect(screen.getByText(/Welcome to our Blood Donation Organization!/i)).toBeInTheDocument();
+    });
+  
+    test('renders the "Why We Help" section with list items', () => {
+      render(<AboutUs />);
+  
+      expect(screen.getByText('Why We Help')).toBeInTheDocument();
+      expect(screen.getByText(/Emergency Situations:/i)).toBeInTheDocument();
+      expect(screen.getByText(/Medical Treatments:/i)).toBeInTheDocument();
+    });
+  
+    test('renders the footer with social media links', () => {
+      render(<AboutUs />);
+  
+      expect(screen.getByLabelText('Facebook')).toBeInTheDocument();
+      expect(screen.getByLabelText('Twitter')).toBeInTheDocument();
+      expect(screen.getByLabelText('Instagram')).toBeInTheDocument();
+      expect(screen.getByText(/© 2023 Your Website. All rights reserved./i)).toBeInTheDocument();
+    });
+  
+    
+  });
+  
