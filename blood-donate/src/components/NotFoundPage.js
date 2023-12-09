986189bb1fbc13692302d924092658ed7b96556f
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-09 11:40:18 +0000

    create custom notfoundpage component

---
+++ blood-donate/src/components/NotFoundPage.js
@@ -0,0 +1,15 @@
+// components/NotFoundPage.js
+
+import React from 'react';
+import '../style/NotFoundPage.css';
+
+const NotFoundPage = () => {
+    return (
+        <div className="not-found-page">
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for might have been removed, or is temporarily unavailable.</p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
