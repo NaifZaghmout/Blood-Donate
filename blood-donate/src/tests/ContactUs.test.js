868c9394e902f714e121b220d597f902e7d1dc7b
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-09 09:42:23 +0000

    add basic content and form field tests for contactus.js

--- blood-donate/src/tests/ContactUs.test.js
+++ blood-donate/src/tests/ContactUs.test.js
@@ -3,3 +3,29 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import ContactUs from '../components/ContactUs';
 
+describe('ContactUs Component', () => {
+    test('renders contact information and message form headings', () => {
+      render(<ContactUs />);
+  
+      expect(screen.getByText('Contact Information')).toBeInTheDocument();
+      expect(screen.getByText('Send Us a Message')).toBeInTheDocument();
+    });
+  
+    test('renders contact information details', () => {
+      render(<ContactUs />);
+  
+      expect(screen.getByText('+1 (555) 123-4567')).toBeInTheDocument();
+      expect(screen.getByText('123 Main Street, Cityville, State, Zip Code')).toBeInTheDocument();
+      expect(screen.getByText('info@blooddonation.org')).toBeInTheDocument();
+    });
+  
+  
+    test('renders form fields with placeholders', () => {
+      render(<ContactUs />);
+  
+      expect(screen.getByPlaceholderText('Enter your name')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Enter your email')).toBeInTheDocument();
+      expect(screen.getByPlaceholderText('Type your message here')).toBeInTheDocument();
+    });
+   
+  });
