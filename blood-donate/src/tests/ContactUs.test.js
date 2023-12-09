0cbed8836243ea1b7681ed7a677a835bdf8c2043
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-09 10:09:09 +0000

    add hover effect and footer tests for contactus.js

--- blood-donate/src/tests/ContactUs.test.js
+++ blood-donate/src/tests/ContactUs.test.js
@@ -19,6 +19,14 @@ describe('ContactUs Component', () => {
       expect(screen.getByText('info@blooddonation.org')).toBeInTheDocument();
     });
   
+    test('check hover effect on contact information section', () => {
+      render(<ContactUs />);
+      const contactInfo = screen.getByText('+1 (555) 123-4567').closest('div');
+      fireEvent.mouseEnter(contactInfo);
+      expect(contactInfo).toHaveStyle('background: purple');
+      fireEvent.mouseLeave(contactInfo);
+      expect(contactInfo).toHaveStyle('background: darkorchid');
+    });
   
     test('renders form fields with placeholders', () => {
       render(<ContactUs />);
@@ -27,5 +35,22 @@ describe('ContactUs Component', () => {
       expect(screen.getByPlaceholderText('Enter your email')).toBeInTheDocument();
       expect(screen.getByPlaceholderText('Type your message here')).toBeInTheDocument();
     });
-   
+  
+    test('check hover effect on message form', () => {
+      render(<ContactUs />);
+      const messageForm = screen.getByPlaceholderText('Enter your name').closest('form');
+      fireEvent.mouseEnter(messageForm);
+      expect(messageForm).toHaveStyle('background: purple');
+      fireEvent.mouseLeave(messageForm);
+      expect(messageForm).toHaveStyle('background: darkorchid');
+    });
+  
+    test('renders the footer with social media links', () => {
+      render(<ContactUs />);
+  
+      expect(screen.getByLabelText('Facebook')).toBeInTheDocument();
+      expect(screen.getByLabelText('Twitter')).toBeInTheDocument();
+      expect(screen.getByLabelText('Instagram')).toBeInTheDocument();
+      expect(screen.getByText(/© 2023 Your Website. All rights reserved./i)).toBeInTheDocument();
+    });
   });
