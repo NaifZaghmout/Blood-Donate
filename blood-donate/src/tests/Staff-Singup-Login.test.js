b74edadfc829f81ca798586ed4f4432730d3172e
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-11 16:19:51 +0000

    update notfoundpage and staff singup-loging pages tests

--- blood-donate/src/tests/Staff-Singup-Login.test.js
+++ blood-donate/src/tests/Staff-Singup-Login.test.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import StaffSignupLogin from '../components/Staff-Singup-Login';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('StaffSignupLogin Component', () => {
-    const setup = (isSignup = true) => {
+    const setup = async (isSignup = true) => {
         render(
             <BrowserRouter>
                 <StaffSignupLogin />
             </BrowserRouter>
         );
 
+        // Wait for the loader to finish
+        await waitFor(() => expect(screen.queryByText('Loading . . .')).not.toBeInTheDocument(), { timeout: 4000 });
+
         if (!isSignup) {
             fireEvent.click(screen.getByText('Login'));
         }
     };
 
-    test('renders signup form by default', () => {
-        setup();
+    test('renders signup form by default', async () => {
+        await setup();
         expect(screen.getByText('Staff Signup')).toBeInTheDocument();
         expect(screen.getByLabelText('Username')).toBeInTheDocument();
         expect(screen.getByLabelText('Password')).toBeInTheDocument();
@@ -26,16 +29,16 @@ describe('StaffSignupLogin Component', () => {
         expect(screen.getByRole('button', { name: 'Signup' })).toBeInTheDocument();
     });
 
-    test('toggles to login form', () => {
-        setup();
+    test('toggles to login form', async () => {
+        await setup();
         fireEvent.click(screen.getByText('Login'));
         expect(screen.getByText('Staff Login')).toBeInTheDocument();
         expect(screen.queryByLabelText('Confirm Password')).not.toBeInTheDocument();
         expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument();
     });
 
-    test('renders login form correctly', () => {
-        setup(false);
+    test('renders login form correctly', async () => {
+        await setup(false);
         expect(screen.getByText('Staff Login')).toBeInTheDocument();
         expect(screen.getByLabelText('Username')).toBeInTheDocument();
         expect(screen.getByLabelText('Password')).toBeInTheDocument();
