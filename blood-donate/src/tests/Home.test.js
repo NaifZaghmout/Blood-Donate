61a596c0969ef96548545f19bf1687f016186fb8
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-12 12:08:32 +0000

    update home test page

--- blood-donate/src/tests/Home.test.js
+++ blood-donate/src/tests/Home.test.js
@@ -1,61 +1,66 @@
 // src/tests/home.test.js
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Home from '../components/Home';
 
 describe('Home Component', () => {
-  beforeEach(() => {
+  const setup = async () => {
     render(<Home />);
+    await waitFor(() => expect(screen.queryByText('Loading . . .')).not.toBeInTheDocument(), { timeout: 5000 });
+  };
+
+  beforeEach(async () => {
+    await setup();
   });
 
-  test('renders welcome message', () => {
-    const welcomeMessage = screen.getByText(/Welcome to Stockholm Blood Donate Organization/i);
+  test('renders welcome message',async () => {
+    const welcomeMessage = await screen.findByText(/Welcome to Stockholm Blood Donate Organization/i);
     expect(welcomeMessage).toBeInTheDocument();
   });
 
-  test('renders blood donation sections', () => {
-    const maleDonationSection = screen.getByText(/Donating Blood as a Male/i);
-    const femaleDonationSection = screen.getByText(/Donating Blood as a Female/i);
+  test('renders blood donation sections',async () => {
+    const maleDonationSection = await screen.findByText(/Donating Blood as a Male/i);
+    const femaleDonationSection = await screen.findByText(/Donating Blood as a Female/i);
     expect(maleDonationSection).toBeInTheDocument();
     expect(femaleDonationSection).toBeInTheDocument();
   });
 
 
-  test('renders the initial image in the carousel', () => {
-    const firstImage = screen.getByAltText(/Image 1/i);
+  test('renders the initial image in the carousel',async () => {
+    const firstImage = await screen.findByAltText(/Image 1/i);
     expect(firstImage).toBeInTheDocument();
   });
 
-  test('renders "Who Can Donate" section correctly', () => {
-    const whoCanDonate = screen.getByText(/Who Can Donate/i);
+  test('renders "Who Can Donate" section correctly', async() => {
+    const whoCanDonate = await screen.findByText(/Who Can Donate/i);
     expect(whoCanDonate).toBeInTheDocument();
   });
 
 
-  test('renders health benefits section correctly', () => {
-    const healthBenefits = screen.getByText(/Health Benefits for Blood Donation/i);
+  test('renders health benefits section correctly',async () => {
+    const healthBenefits = await screen.findByText(/Health Benefits for Blood Donation/i);
     expect(healthBenefits).toBeInTheDocument();
   });
 
-  test('renders footer content', () => {
-    const footerContent = screen.getByText(/2023 Your Website. All rights reserved./i);
+  test('renders footer content', async() => {
+    const footerContent = await screen.findByText(/2023 Your Website. All rights reserved./i);
     expect(footerContent).toBeInTheDocument();
   });
 
 
-  test('renders Facebook social media link correctly', () => {
-    const facebookLink = screen.getByRole('link', { name: /facebook/i });
+  test('renders Facebook social media link correctly',async () => {
+    const facebookLink = await screen.findByRole('link', { name: /facebook/i });
     expect(facebookLink).toHaveAttribute('href', 'https://www.facebook.com');
   });
 
-  test('renders Twitter social media link correctly', () => {
-    const twitterLink = screen.getByRole('link', { name: /twitter/i });
+  test('renders Twitter social media link correctly', async() => {
+    const twitterLink = await screen.findByRole('link', { name: /twitter/i });
     expect(twitterLink).toHaveAttribute('href', 'https://www.twitter.com');
   });
 
-  test('renders Instagram social media link correctly', () => {
-    const instagramLink = screen.getByRole('link', { name: /instagram/i });
+  test('renders Instagram social media link correctly',async () => {
+    const instagramLink = await screen.findByRole('link', { name: /instagram/i });
     expect(instagramLink).toHaveAttribute('href', 'https://www.instagram.com');
   });
 
