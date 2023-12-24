93c54bdf9cbafd950b50309be436bf1f0941cc16
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-24 13:55:03 +0000

    add handleSubmit method and render signup form

--- blood-donate/src/components/Staff-Singup.js
+++ blood-donate/src/components/Staff-Singup.js
@@ -50,7 +50,112 @@ const Signup = () => {
         handleChange(e);
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const apiUrl = 'https://8000-naifzaghmou-blooddonate-8h80369qfat.ws-us107.gitpod.io/api/register';
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            if (response.ok) {
+                window.location.href = '/';
+            } else {
+                console.error('Signup failed');
+            }
+        } catch (error) {
+            console.error('Error during signup:', error);
+        }
+    };
 
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    return (
+        <Container className="mt-5 custom-container">
+            <h1 className="signup-heading">Staff Sign-Up</h1>
+            <Form className="custom-form" onSubmit={handleSubmit}>
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        className="custom-form-control"
+                        type="email"
+                        placeholder="Enter your email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        className="custom-form-control"
+                        type="text"
+                        placeholder="Choose a username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        className={`custom-form-control ${!isPasswordValid ? 'invalid-password' : ''}`}
+                        type="password"
+                        placeholder="Enter your password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handlePasswordChange}
+                        required
+                    />
+                    {!isPasswordValid && (
+                        <div className="password-warning">
+                            <small>
+                                Password must be at least 8 characters long and not entirely numeric.
+                            </small>
+                        </div>
+                    )}
+                </Form.Group>
+
+                <Form.Group controlId="formConfirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        className="custom-form-control"
+                        type="password"
+                        placeholder="Confirm your password"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={handleConfirmPasswordChange}
+                        required
+                    />
+                    {!isConfirmPasswordValid && (
+                        <div className="validation-message">
+                            Passwords do not match.
+                        </div>
+                    )}
+                </Form.Group>
+
+                <Button
+                    className="custom-button"
+                    variant="primary"
+                    type="submit"
+                    disabled={!isPasswordValid || !isConfirmPasswordValid}>
+                    Sign Up
+                </Button>
+            </Form>
+        </Container>
+    );
 };
+
+
+
 
 export default Signup;
