0397117edd026a091a31b53663d9703fc7c440f3
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2023-12-24 17:31:44 +0000

    implement render method for login component

--- blood-donate/src/components/Staff-Login.js
+++ blood-donate/src/components/Staff-Login.js
@@ -8,13 +8,13 @@ import Loader from './Loader';
 const Login = () => {
     const [formData, setFormData] = useState({
         email: '',
-        username: '', 
+        username: '',
         password: '',
     });
 
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState('');
-    
+
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(false);
@@ -43,9 +43,68 @@ const Login = () => {
             setError('An error occurred during login.');
         }
     };
-    
-    
-    
+
+    if (isLoading) {
+        return <Loader />;
+    }
+
+    return (
+        <Container className="mt-5 custom-container">
+            <h1 className="login-heading">Staff Login</h1>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form className="custom-form" onSubmit={handleSubmit}>
+                <Form.Group controlId="formUsername">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                        className="custom-form-control"
+                        type="text"
+                        placeholder="Enter your username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                        required
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        className="custom-form-control"
+                        type="email"
+                        placeholder="Enter your email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </Form.Group>
+
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        className="custom-form-control"
+                        type="password"
+                        placeholder="Enter your password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                        required
+                    />
+                </Form.Group>
+
+                <Button
+                    className="custom-button"
+                    variant="primary"
+                    type="submit">
+                    Login
+                </Button>
+                <div className="login-signup-link">
+                    Don't have an account? <Link to="/staff-signup">Sign up here</Link>
+                </div>
+            </Form>
+
+        </Container>
+    );
 };
 
 export default Login;
