f09a3bde1a3abc097bc3076be4264af2294c72f9
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2024-01-05 11:59:03 +0000

    api changed

--- blood-donate/src/api/axiosDefaults.js
+++ blood-donate/src/api/axiosDefaults.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://stockholm-blood-donate-organization.onrender.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
