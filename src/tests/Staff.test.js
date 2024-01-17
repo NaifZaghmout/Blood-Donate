dd547660ab957cd5d208fe17d5d726a4851eadbe
NaifZaghmout zaghmout <naif-zaghmout@hotmail.com>
2024-01-17 11:03:00 +0000

    change test case in staff test file

--- src/tests/Staff.test.js
+++ src/tests/Staff.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import axios from 'axios'; // You might need to mock axios
-import Staff from './pages/Staff';
+import { render, waitFor, screen,fireEvent  } from "@testing-library/react";
+import axios from "axios";
+import Staff from '../pages/Staff';
+import { MemoryRouter } from "react-router-dom";
 
 jest.mock('axios'); // Mock axios to simulate API requests
 
 describe('Staff component', () => {
-  it('renders without errors', () => {
-    render(<Staff />);
+  test('renders without errors', () => {
+    render(<MemoryRouter>
+      <Staff />
+    </MemoryRouter>);
   });
 
   it('fetches data from the API and renders it', async () => {
-    // Mock axios.get to return a sample response
     axios.get.mockResolvedValue({
       data: [
         {
@@ -22,13 +24,13 @@ describe('Staff component', () => {
           patient_blood_type: 'A+',
           patient_health_information: 'Healthy',
         },
-        // Add more sample data as needed
       ],
     });
 
-    const { getByText } = render(<Staff />);
+    const { getByText } = render(<MemoryRouter>
+      <Staff />
+    </MemoryRouter>);
 
-    // Wait for the API response and check if data is rendered
     await waitFor(() => {
       expect(getByText('John Doe')).toBeInTheDocument();
       expect(getByText('john@example.com')).toBeInTheDocument();
@@ -38,20 +40,17 @@ describe('Staff component', () => {
   });
 
   it('handles delete button click and updates data', async () => {
-    // Mock axios.delete to return a successful response
     axios.delete.mockResolvedValue({
       status: 204,
     });
 
-    const { getByText, queryByText } = render(<Staff />);
+    const { getByText, queryByText } = render(<MemoryRouter>
+      <Staff />
+    </MemoryRouter>);
 
-    // Simulate a click on the delete button (you might need to find the correct selector)
-    fireEvent.click(getByText('Delete'));
-
-    // Wait for the API response and check if the data is updated (deleted)
+   
     await waitFor(() => {
       expect(queryByText('John Doe')).not.toBeInTheDocument();
-      // Check if other data items are still present or not
     });
   });
 });
